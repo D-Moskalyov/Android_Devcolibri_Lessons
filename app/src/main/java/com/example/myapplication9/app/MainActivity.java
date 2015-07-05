7511427876a91f10569b3093884056d7256f99ed
D-Moskalyov <moscalevd@Gmail.com>
2015-07-05 22:03:02 +0300

    Hello, World

---
+++ app/src/main/java/com/example/myapplication9/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication9.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
