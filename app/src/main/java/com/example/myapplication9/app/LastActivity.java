769d8cf0be7edad41bb3f2cf73eaae524df76061
D-Moskalyov <moscalevd@Gmail.com>
2015-07-06 22:08:31 +0300

    FullLesson7

--- app/src/main/java/com/example/myapplication9/app/LastActivity.java
+++ app/src/main/java/com/example/myapplication9/app/LastActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LastActivity extends Activity {
+
+    private TextView txtLogin;
+    private TextView txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.last_layout);
+
+        txtLogin = (TextView)findViewById(R.id.txtLogin);
+        txtPassword = (TextView)findViewById(R.id.txtPassword);
+
+        txtLogin.setText(getIntent().getStringExtra("login"));
+        txtPassword.setText(getIntent().getStringExtra("password"));
 
     }
 
@@ -18,4 +28,5 @@ public class LastActivity extends Activity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
