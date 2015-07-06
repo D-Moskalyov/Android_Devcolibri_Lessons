0841607d5b88e29e73823b823a7059f4d490019a
D-Moskalyov <moscalevd@Gmail.com>
2015-07-06 20:25:35 +0300

    FullLesson

--- app/src/main/java/com/example/myapplication9/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/app/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.myapplication9.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Привет, Мир!");
+            }
+        });
+    }
+
+    public void seyHello(View view) {
+        textView.setText("Привет!");
     }
 }
