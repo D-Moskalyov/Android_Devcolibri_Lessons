59caebdfc0fb1f5310ada84e8f6c5309704fe7d7
D-Moskalyov <moscalevd@Gmail.com>
2015-07-09 20:57:59 +0300

    FullLesson16

--- app/src/main/java/com/example/myapplication9/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/app/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.myapplication9.app;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import com.example.myapplication9.app.R;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends Activity{
 
+public class MainActivity extends ActionBarActivity {
+
+    private final String TAG = "DEV";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.activity_main);
+    }
 
+    public void onInfoLog(View view){
+        Log.i(TAG, "Info Level");
+    }
+
+    public void onWarnLog(View view){
+        Log.w(TAG, "Warn Level");
+    }
+
+    public void onErrorLog(View view){
+        Log.e(TAG, "Error Level");
+    }
+
+    public void onDebugLog(View view){
+        Log.d(TAG, "Debug Level");
+        Log.v(TAG, "Verbose Level");
+        Log.wtf(TAG, "WTF Level");
     }
 
 }
