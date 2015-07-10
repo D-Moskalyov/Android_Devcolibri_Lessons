f5db0fb993d256d4760b6f3553b3f8639cfb2b2c
D-Moskalyov <moscalevd@Gmail.com>
2015-07-10 22:53:29 +0300

    FullLesson22

---
+++ app/src/main/java/com/example/myapplication9/app/ButtonFragment.java
@@ -0,0 +1,17 @@
+package com.example.myapplication9.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ButtonFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.button_layout, container, false);
+    }
+}
--- app/src/main/java/com/example/myapplication9/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/app/MainActivity.java
@@ -1,49 +1,13 @@
 package com.example.myapplication9.app;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-import com.example.myapplication9.app.R;
+import android.support.v4.app.FragmentActivity;
 
-import java.util.*;
-
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_items, menu);
-        return true;
-    }
-
-    public void onAbout(MenuItem item){
-        Toast.makeText(this, "об авторе", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.settings:
-                Toast.makeText(this, "Вы выбралм настройки", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.about:
-                Toast.makeText(this, "Вы выбралм об авторе", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.site:
-                Toast.makeText(this, "Вы выбралм настройки", Toast.LENGTH_SHORT).show();
-                break;
-            default:break;
-        }
-
-        return true;
     }
 }
