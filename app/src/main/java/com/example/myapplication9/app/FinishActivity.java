a2b0af06bf6429dea7a90bf823b72c3d0c3d3388
D-Moskalyov <moscalevd@Gmail.com>
2015-07-10 21:49:26 +0300

    FullLesson20

---
+++ app/src/main/java/com/example/myapplication9/app/FinishActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication9.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinishActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finish_layout);
+    }
+}
