9ffaca499a68dcf02a7a961420e146a3487225fe
D-Moskalyov <moscalevd@Gmail.com>
2015-07-13 22:43:03 +0300

    FullLesson31

--- app/src/main/java/com/example/myapplication9/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/app/MainActivity.java
@@ -1,49 +1,19 @@
 package com.example.myapplication9.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import com.example.myapplication9.app.R;
 
-import com.example.myapplication9.app.asynctask.MyAsyncTask;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends Activity {
 
-    private MyAsyncTask task;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //task = new MyAsyncTask(this);
-        task = MyAsyncTask.getInstance(this);
-        //task.execute();
+        startService(new Intent(this, MyService.class));
     }
 
 
-    public void onStartTask(View view) {
-
-//        task.execute();
-
-    }
-
-    public void onCancelTask(View view) {
-
-        task.cancel(false);
-    }
-
-    public void onStatus(View view) {
-
-        Toast.makeText(this, task.getStatus().toString(), Toast.LENGTH_SHORT).show();
-    }
 }
---
+++ app/src/main/java/com/example/myapplication9/app/MyService.java
@@ -0,0 +1,20 @@
+package com.example.myapplication9.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("START_SERVICE", "Service is running");
+    }
+}
